38e8964c45bae459b175c9dcf0c635365aad7bc7
danwebb <danwebb@a686c101-5f17-0410-80e1-b267258107f6>
2006-07-25 16:02:51 +0000

    git-svn-id: http://svn.danwebb.net/external/lowpro/trunk@7 a686c101-5f17-0410-80e1-b267258107f6

---
+++ src/eventext.js
@@ -0,0 +1,24 @@
+// Extend Element with observe and stopObserving.
+Element.addMethods({
+  
+  observe : function(el, event, callback) {
+    Event.observe.call(Event, el, event, callback);
+  },
+  
+  stopObserving : function(el, event, callback) {
+    Event.stopObserving.call(Event, el, event, callback);
+  }
+  
+});
+
+// Allows you to trigger an event element.  
+Object.extend(Event, {
+  trigger : function(element, event, fakeEvent) {
+    element = $(element);
+    fakeEvent = fakeEvent || { type :  event };
+    this.observers.each(function(cache) {
+      if (cache[0] == element && cache[1] == event)
+        cache[2].call(element, fakeEvent);
+    })
+  }
+});
