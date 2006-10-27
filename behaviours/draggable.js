5598920922d6960c5dc7850d5e85c43cc97f1062
danwebb <danwebb@a686c101-5f17-0410-80e1-b267258107f6>
2006-10-27 06:48:37 +0000

    added start of a draggable implementation to experiment with behaviour classes
    git-svn-id: http://svn.danwebb.net/external/lowpro/trunk@133 a686c101-5f17-0410-80e1-b267258107f6

---
+++ behaviours/draggable.js
@@ -0,0 +1,25 @@
+Draggable = Behavior.create({
+  initialize : function(options) {
+    this.options = Object.extend({
+      // defaults
+    }, options);
+  },
+  
+  onmousedown : function() {
+    
+  },
+  
+  onmouseover : function() {
+    
+  },
+  
+  onmousemove : function() {
+    
+  }
+});
+
+Object.extend(Draggable, {
+  all : [],
+  register : function() {},
+  unregister : function() {}
+});
