1869e6308817a96725005fa5e975048fd1766e51
danwebb <danwebb@a686c101-5f17-0410-80e1-b267258107f6>
2007-12-12 22:20:21 +0000

    fixed stupid version warning mistake
    git-svn-id: http://svn.danwebb.net/external/lowpro/trunk@344 a686c101-5f17-0410-80e1-b267258107f6

--- src/lowpro.js
+++ src/lowpro.js
@@ -2,7 +2,7 @@ LowPro = {};
 LowPro.Version = '0.5';
 LowPro.CompatibleWithPrototype = '1.6';
 
-if (Prototype.Version.indexOf(LowPro.CompatibleWithPrototype) == 0 && console && console.warn)
+if (Prototype.Version.indexOf(LowPro.CompatibleWithPrototype) != 0 && console && console.warn)
   console.warn("This version of Low Pro is tested with Prototype " + LowPro.CompatibleWithPrototype + 
                   " it may not work as expected with this version (" + Prototype.Version + ")");
 
